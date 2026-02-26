c8032fdc53887fb9e18ed333c9a800d2ddcdafb5
Nekopazo <yxm1010122195@gmail.com>
2026-02-26 23:17:34 +0800

    Update 1.sh

--- DiffuFont/1.sh
+++ DiffuFont/1.sh
@@ -5,22 +5,4 @@ ROOT="DiffuFont/"
 
 cd DiffuFont
 
-python DataPreparation/generate_font_images.py \
-    --project-root "DiffuFont/" \
-    --char-list-json DiffuFont/CharacterData/CharList.json \
-    --font-list-json DiffuFont/DataPreparation/ContentFontList.json \
-    --font-dir DiffuFont/DataPreparation/Font \
-    --out-dir DiffuFont/DataPreparation/Generated/ContentFont
 
-python DataPreparation/generate_font_images.py \
-    --project-root "DiffuFont/" \
-    --char-list-json DiffuFont/CharacterData/CharList.json \
-    --font-list-json DiffuFont/DataPreparation/FontList.json \
-    --font-dir DiffuFont/DataPreparation/Font \
-    --out-dir DiffuFont/DataPreparation/Generated/TrainFonts
-    
-python DataPreparation/images_to_lmdb.py \
-    --project-root "DiffuFont/" \
-    --img-roots DiffuFont/DataPreparation/Generated/ContentFont \
-    --lmdb-path DiffuFont/DataPreparation/LMDB/ContentFont.lmdb \
-    --overwrite
