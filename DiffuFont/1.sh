6e07ecbbc569f6780f5efc986868a5c0d2c77f9f
Nekopazo <yxm1010122195@gmail.com>
2026-02-27 01:14:59 +0800

    Update 1.sh

--- DiffuFont/1.sh
+++ DiffuFont/1.sh
@@ -54,7 +54,7 @@ python DiffuFont/scripts/build_part_bank_component_aware_from_images.py \
     --glyph-root DataPreparation/Generated/TrainFonts \
     --output-dir DataPreparation/PartBank \
     --parts-per-font 32 \
-    --reference-char-json Character/ReferenceCharList_200_balanced_v3.json\
+    --reference-char-json CharacterData/ReferenceCharList_200_balanced_v3.json \
     --workers 4
 
 echo "=========================================="
